2a97cfead8810191986f16a52e54e51cde8db112
jingyu9804 <swc05161@gmail.com>
2023-03-28 17:43:34 +0900

    feat(jingyulog) : 카테고리 목록 Props로 받아 띄워주기

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -1,1 +1,35 @@
 // 카테고리 목록
+import React, { FunctionComponent } from "react";
+import styled from "@emotion/styled";
+
+export type CategoryListProps = {
+    selectedCategory: string
+    categoryList: {
+        // 프로퍼티 이름은 문자열, 프로퍼티 값은 숫자임을 나타내느 타입 표기법
+        [key: string]: number
+    }
+}
+
+const CategoryListWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 768px;
+    margin: 100px auto 0;
+`
+
+const CategoryList: FunctionComponent<CategoryListProps> = function ({
+    selectedCategory,
+    categoryList,
+}) {
+    return (
+        <CategoryListWrapper>
+            {Object.entries(categoryList).map(([name, count]) => (
+                <div key={name}>
+                    #{name}({count})
+                </div>
+            ))}
+        </CategoryListWrapper>
+    )
+}
+
+export default CategoryList
