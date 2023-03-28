000108719839dba8ddd85dfebdf6efa2628611f7
jingyu9804 <swc05161@gmail.com>
2023-03-28 18:04:19 +0900

    feat(jingyulog) : 카테고리 목록 컴포넌트 구현

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -1,6 +1,17 @@
 // 카테고리 목록
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, ReactNode } from "react";
 import styled from "@emotion/styled";
+import { Link } from "gatsby";
+
+type CategoryItemProps = {
+    active: boolean
+}
+
+type GatsbyLinkProps = {
+    children: ReactNode
+    className?: string
+    to: string
+} & CategoryItemProps
 
 export type CategoryListProps = {
     selectedCategory: string
@@ -17,6 +28,20 @@ const CategoryListWrapper = styled.div`
     margin: 100px auto 0;
 `
 
+const CategoryItem = styled(({active, ...props}: GatsbyLinkProps) => (
+    <Link {...props} />
+))<CategoryItemProps>`
+    margin-right: 20px;
+    padding: 5px 0;
+    font-size: 18px;
+    font-weight: ${({active}) => (active ? '800' : '400')};
+    cursor: pointer;
+
+    &:last-of-type{
+        margin-right: 0;
+    }
+`
+
 const CategoryList: FunctionComponent<CategoryListProps> = function ({
     selectedCategory,
     categoryList,
@@ -24,9 +49,13 @@ const CategoryList: FunctionComponent<CategoryListProps> = function ({
     return (
         <CategoryListWrapper>
             {Object.entries(categoryList).map(([name, count]) => (
-                <div key={name}>
+                <CategoryItem
+                    to={`/?category=${name}`}
+                    active={name === selectedCategory}
+                    key={name}
+                >
                     #{name}({count})
-                </div>
+                </CategoryItem>
             ))}
         </CategoryListWrapper>
     )
