e0699348eb485dcf95cde08bc2f83417cd75f4fe
jingyu9804 <swc05161@gmail.com>
2023-03-28 16:17:00 +0900

    feat(jingyulog) : EmotionJS 라이브러리를 통한 컴포넌트 스타일링 방법

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
-import Text from 'components/Text'
+import styled from '@emotion/styled'
+import { Global, css } from '@emotion/react'
 
 type InfoPageProps = {
   data: {
@@ -14,6 +15,35 @@ type InfoPageProps = {
   }
 }
 
+const globalStyle = css`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+        font-size: 20px;
+    }
+`
+
+const TextStyle = css`
+    font-size: 18px;
+    font-weight: 700;
+    color: gray;
+`
+// Kebab Case 적용
+const Text1 = styled.div<{ disable: boolean }>`
+    font-size: 20px;
+    font-weight: 700;
+    text-decoration: ${({disable}) => (disable ? 'line-through' : 'none')};
+`
+
+// Camel Case 적용
+const Text2 = styled('div')<{ disable: boolean }>(({ disable }) => ({
+    fontSize: '15px',
+    color: 'blue',
+    textDecoration: disable ? 'line-through' : 'none',
+}))
+
 const InfoPage: FunctionComponent<InfoPageProps> = function ({
   data: {
     site: {
@@ -23,9 +53,10 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
 }) {
   return (
     <div>
-      <Text text={title} />
-      <Text text={description} />
-      <Text text={author} />
+        <Global styles={globalStyle} />
+        <div css={TextStyle}>{title}</div> 
+        <Text1 disable={true}>{description}</Text1>
+        <Text2 disable={true}>{author}</Text2>
     </div>
   )
 }
@@ -42,4 +73,4 @@ export const metadataQuery = graphql`
       }
     }
   }
-`;
+`
