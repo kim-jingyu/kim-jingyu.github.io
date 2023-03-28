faa61ee748ffc2c07a02f61ee2b1957cf3f9dc89
jingyu9804 <swc05161@gmail.com>
2023-03-28 17:15:51 +0900

    feat(jingyulog) : 메인 페이지 상단

--- src/components/Main/Introduction.tsx
+++ src/components/Main/Introduction.tsx
@@ -1,1 +1,48 @@
 // 페이지 상단 소개글
+import React, {FunctionComponent} from "react";
+import styled from "@emotion/styled";
+import ProfileImage from "./ProfileImage";
+
+const BackGround = styled.div`
+    width: 100%;
+    background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
+    color: #ffffff;
+`
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 768px;
+    height: 400px;
+    margin: 0 auto;
+`
+
+const Subtitle = styled.div`
+    font-size: 20px;
+    font-weight: 400;
+`
+
+const Title = styled.div`
+    margin-top: 5px;
+    font-size: 35px;
+    font-weight: 700;
+`
+
+const Introduction: FunctionComponent = function () {
+    return(
+        <BackGround>
+            <Wrapper>
+                <ProfileImage />
+
+                <div>
+                    <Subtitle>Nice to Meet You!!</Subtitle>
+                    <Title>I'm Jingyu!!</Title>
+                </div>
+            </Wrapper>
+        </BackGround>
+    )
+}
+
+export default Introduction
--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -3,4 +3,17 @@ import React, {FunctionComponent} from "react"
 import styled from "@emotion/styled"
 
 // 프로필 이미지 링크 설정
-const PROFILE_IMAGE_LINK = '<>'
+const PROFILE_IMAGE_LINK = 'https://raw.githubusercontent.com/jingyu9804/jingyu9804.github.io/develop/images/profile.jpeg'
+
+const ProfileImageWrapper = styled.img`
+    width: 120px;
+    height: 120px;
+    margin-bottom: 30px;
+    border-radius: 50%;
+`
+
+const ProfileImage: FunctionComponent = function () {
+    return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+}
+
+export default ProfileImage
